5851fe20d1468ccdee97b53e8f776ca88ee1ae13
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2021-09-23 13:47:58 +0200

    add obscur vnc password

--- oc/od/vnc_password.py
+++ oc/od/vnc_password.py
@@ -11,41 +11,59 @@
 # Author: abcdesktop.io team
 # Software description: cloud native desktop service
 #
-
-import oc.logging
-import oc.od.settings
-import oc.auth.namedlib
 import oc.lib
-
 from Crypto.Util.strxor import strxor
 import base64
 
-class ODVncPassword(object):
-
-    def __init__(self, initvncpassword=None) -> None:
+class ODVncPassword():
+    """[ODVncPassword]
+        ODVncPassword class to obscur vnc password 
+        this is not a encrypt password
+        the goal is only obscur vnc password 
+    """
+    def __init__(self, key, initvncpassword=None, passwordlen=10) -> None:
+        """[__init__]
+            init ODVncPassword class
+        Args:
+            key ([str]): [cypher key]
+            initvncpassword ([str], optional): [default vnc password value]. Defaults to None.
+            passwordlen (int, optional): [len of the vnc password if it does not exist]. Defaults to 10.
+        """
         super().__init__()
-        self._passlength = 10
-        self._key = oc.od.settings.desktopvnccypherkey
+        self._passlength = passwordlen
+        self._key = key
         self._vncpassword = initvncpassword
 
     def make( self ):
+        """[make]
+            build a vnc password plain text using random value for length 
+            self._passlength = 10 bydefault
+        Returns:
+            [type]: [description]
+        """
         self._vncpassword = oc.lib.randomStringwithDigitsAndSymbols( self._passlength )
         return self._vncpassword
-
-
-
 
     # func to restore padding
     @staticmethod
     def repad(data):
-     return data + "=" * (-len(data)%4)
+        """[repad]
+
+        Args:
+            data ([str]): [string without base64 pad]
+
+        Returns:
+            [str]: [string with base64 pad if need ]
+        """
+        return data + "=" * (-len(data)%4)
 
     def encrypt( self ):
         """[encrypt]
             only obscuring
-            encrypt is not to encrypt, use simple xor to obscur data
+            encrypt is not to encrypt, 
+            use simple xor to obscur data
         Returns:
-            [str]: [description]
+            [str]: [xor encrypted password base64 formated]
         """
         if self._vncpassword is None:
             self.make()
@@ -59,6 +77,17 @@ class ODVncPassword(object):
         return strencrypt
 
     def decrypt( self, ciphertext ):
+        """[decrypt]
+            only obscuring
+            decrypt is not to encrypt, 
+            decrypt decoded xor string
+  
+        Args:
+            ciphertext ([str]): [cypher xor base64 formated ]
+
+        Returns:
+            [str]: [plain text data]
+        """
         ciphertextb64 = self.repad( ciphertext )
         ciphertextb64 = str.encode( ciphertextb64 )
         ciphertext =  base64.b64decode( ciphertextb64 )
@@ -69,6 +98,11 @@ class ODVncPassword(object):
         return self._vncpassword
 
     def getplain( self ):
+        """[getplain]
+            get vnc password plain text
+        Returns:
+            [str]: [clear vnc password plain text]
+        """
         if self._vncpassword is None:
             self.make()
         return self._vncpassword
