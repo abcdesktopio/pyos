5916042456ca404af474d0f42d3185eb614daf6e
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2022-05-10 11:06:01 +0200

    remove desktop if create failed

--- controllers/composer_controller.py
+++ controllers/composer_controller.py
@@ -360,7 +360,9 @@ class ComposerController(BaseController):
             if not hasattr( desktop, 'internaluri') or desktop.internaluri is None:   
                 # a desktop exists but is unreachable
                 # decide to trash it
-                oc.od.composer.removedesktop( auth, user )              
+                services.messageinfo.push(user.userid, 'Your desktop is crashing. Delete desktop process is starting') 
+                oc.od.composer.removedesktop( auth, user )            
+                services.messageinfo.push(user.userid, 'Your desktop is crashing. Delete desktop done.')   
                 return Results.error('Desktop URI is None, creation failed')
             
             # build a jwt token with desktop.internaluri
