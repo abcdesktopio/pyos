372e2ce10938d336a65e2c3c0e65f732fb97d416
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2022-05-18 11:54:40 +0200

    fix error in deprecated image format

--- oc/od/apps.py
+++ oc/od/apps.py
@@ -317,10 +317,11 @@ class ODApps:
         # read the CMD with fallback for compatibiliy with old version release
         cmd = inspect_dict.get('Config').get('Cmd', '/composer/appli-dockerentrypoint.sh' )
         if isinstance( cmd, list ):
-            # fix error
+            # fix error in deprecated image format in release 2.0
             if cmd[0] == 'bash' and len(cmd) == 1:
-                 cmd = '/composer/appli-dockerentrypoint.sh'
-                 
+                cmd.append('/composer/appli-docker-entrypoint.sh')
+                self.logger.warning( f"fixing cmd entry for image {imageid} update to {cmd}")
+
         # read WORKING with fallback for compatibiliy with old version release
         workingdir = inspect_dict.get('Config').get('WorkingDir', oc.od.settings.getballoon_homedirectory() ) 
         # read USER with fallback for compatibiliy with old version release
@@ -426,8 +427,8 @@ class ODApps:
             try:
                 myapp = self.imagetoapp(image)
                 if type(myapp) is dict:
-                    self.logger.debug (f"adding new image {myapp['id']}")
-                    self.logger.debug( f"cmd {myapp['cmd']}" )
+                    # self.logger.debug (f"adding new image {myapp['id']}")
+                    # self.logger.debug( f"cmd {myapp['cmd']}" )
                     mydict[ myapp['id'] ] = myapp
             except Exception as e:
                 self.logger.error('Image id:%s failed invalid value: %s', image, e)
