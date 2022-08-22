79e1cf59dfb5d412cf1a9e1eb6fa6d0c317a028d
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2022-08-22 12:29:53 +0200

    refactoring to remove dockershim

--- oc/od/kuberneteswatcher.py
+++ oc/od/kuberneteswatcher.py
@@ -20,10 +20,12 @@ class ODKubernetesWatcher:
     def loopforevent( self ):
         self.logger.debug('' )
         self.watch = watch.Watch() 
+        self.logger.debug('loopforevent start inifity loop')
         while( True ): #  inifity loop stop when watch.stop     
             try:
                 # watch list_namespaced_pod waiting for a valid ip addr          
                 events = self.watch.stream(  self.orchestrator.kubeapi.list_namespaced_pod, namespace=self.orchestrator.namespace, timeout_seconds=self.DEFAULT_K8S_WATCHER_TIMEOUT_SECONDS)
+                # events = self.watch.stream(  self.orchestrator.kubeapi.list_namespaced_ , namespace=self.orchestrator.namespace, timeout_seconds=self.DEFAULT_K8S_WATCHER_TIMEOUT_SECONDS)
                 if self.watch._stop :
                     return  # stop this thread 
                 for event in events:
@@ -34,14 +36,15 @@ class ODKubernetesWatcher:
 
                     # event dict must contain a type 
                     event_type = event.get('type')
+                    # self.logger.debug( f"event {event_type} received" )
+
                     if event_type == 'MODIFIED':
-                        self.logger.debug( f"event {event_type} receive" )
                         # event dict must contain a object 
                         pod_event = event.get('object')
                         # if podevent type is pod
                         if isinstance( pod_event, client.models.v1_pod.V1Pod ) : 
                             self.logger.debug( f"{event_type} -> {pod_event.metadata.name}" )
-                            podtype = pod_event.metadata.labels.get( 'type' )
+                            podtype = pod_event.metadata.labels.get('type')
                             if podtype == self.orchestrator.applicationtype :
                                 self.logger.debug( f"{event_type} -> {pod_event.metadata.name}:{podtype}" )
                                 if isinstance( pod_event.status, client.models.v1_pod_status.V1PodStatus ):
@@ -51,10 +54,12 @@ class ODKubernetesWatcher:
                                     if isinstance( state.terminated, client.models.v1_container_state_terminated.V1ContainerStateTerminated ):
                                         self.logger.debug( f"{event_type} -> {pod_event.metadata.name} phase: {pod_event.status.phase} reason:{state.terminated.reason}" )
                                         if state.terminated.reason == 'Completed':
-                                            self.orchestrator.removePod( pod_event )
+                                            # the pod is terminated status is 'Completed'
+                                            # pod_event.status.phase == 'Succeeded' or
+                                            if pod_event.status.phase == 'Running':
+                                                self.orchestrator.removePod( pod_event )
 
-                    if event_type == 'DELETED':
-                        self.logger.debug( f"event {event_type} receive" )
+                    elif event_type == 'DELETED':
                         # event dict must contain a object 
                         pod_event = event.get('object')
                         # if podevent type is pod
@@ -65,6 +70,8 @@ class ODKubernetesWatcher:
                                 self.logger.debug( f"{event_type} -> {pod_event.metadata.name}:{podtype}" )
                                 desktop = self.orchestrator.pod2desktop( pod_event )
                                 oc.od.composer.detach_container_from_network(desktop.name)
+
+
             except Exception as e:
                 self.logger.debug( e )
                 pass
