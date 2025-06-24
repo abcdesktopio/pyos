a3dc5f4e54b98ea7f34a5a41e231fe069e47076c
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2025-06-24 10:23:53 +0200

    add signal handler

--- od.py
+++ od.py
@@ -20,6 +20,7 @@ import os
 import cherrypy # web framework 
 from cherrypy._cpdispatch import Dispatcher
 from cherrypy.process import plugins
+from cherrypy.process.plugins import SignalHandler
 
 import oc.logging
 import oc.cherrypy
@@ -197,19 +198,30 @@ class API(object):
     def healthz(self):
         # disable trace response in log
         cherrypy.response.notrace = True
-        # return OK i'm fine
         return "OK"  
     
     
 class ODCherryWatcher(plugins.SimplePlugin):
     """ signal thread to stop when cherrypy stop"""
     def start(self):
-        # logger.debug( "ODCherryWatcher start events" )
+        logger.debug( "ODCherryWatcher start events" )
         oc.od.services.services.start()
 
     def stop(self):
-        # logger.debug("ODCherryWatcher is stopping. Stopping runnging thread")
+        logger.debug("ODCherryWatcher is stopping. Stopping runnging thread")
         oc.od.services.services.stop()
+
+def handler_SIGNAL( signal:str, **signum )->None:
+    logger.info(f"*** Received signal {signal}, stopping cherrypy engine and services {len(signum)}")
+    # stop services
+    oc.od.services.services.stop()
+    # stop cherrypy engine
+    cherrypy.engine.exit()
+
+def handler_SIGQUIT( **signum ): handler_SIGNAL( 'SIGQUIT', **signum )
+def handler_SIGINT ( **signum ): handler_SIGNAL( 'SIGINT' , **signum )
+def handler_SIGTERM( **signum ): handler_SIGNAL( 'SIGTERM', **signum )
+def handler_SIGSTOP( **signum ): handler_SIGNAL( 'SIGSTOP', **signum )
 
 
 def run_server():
@@ -217,6 +229,15 @@ def run_server():
     # update config for cherrypy with the od.config file
     cherrypy.config.update(settings.get_configuration_file_name())
     logger.debug(f"cherrypy.config.update({settings.get_configuration_file_name()}) done")  
+
+    # signal handler 
+    signalhandler = SignalHandler(cherrypy.engine)
+    signalhandler.handlers['SIGTERM'] = handler_SIGTERM
+    signalhandler.handlers['SIGQUIT'] = handler_SIGQUIT
+    signalhandler.handlers['SIGINT'] = handler_SIGINT
+    # signalhandler.handlers['SIGSTOP'] = handler_SIGSTOP
+    signalhandler.subscribe()
+    
     # set auth tools
     cherrypy.tools.auth = services.services.auth
     # set /API
@@ -228,6 +249,7 @@ def run_server():
     cherrypy.engine.start()
     # infite loop
     logger.info("Waiting for requests.")
+
     cherrypy.engine.block()
 
 def main(argv):
