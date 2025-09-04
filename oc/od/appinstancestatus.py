7012c0109ee9cf9bb5046cf9eca6229e56f23875
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2025-09-04 14:36:52 +0200

    add icon

--- oc/od/appinstancestatus.py
+++ oc/od/appinstancestatus.py
@@ -15,15 +15,24 @@
 # -*- coding: utf-8 -*-
 
 class ODAppInstanceStatus():
-    def __init__(self, message=None, id=None, webhook=None, type=None, wm_class=None):
+    def __init__(self, message:str=None, id:str=None, webhook:str=None, type:str=None, wm_class:str=None, icon:str=None, icondata:str=None):
         self.message = message
         self.id = id
         self.webhook = webhook
         self.type=type
         self.wm_class = wm_class
+        self.icon = icon
+        self.icondata = icondata
 
     def to_dict( self ):
-        return { 'container_id': self.id, 'state': self.message, 'type': self.type, 'wm_class': self.wm_class }
+        return { 
+            'container_id': self.id, 
+            'state': self.message, 
+            'type': self.type, 
+            'wm_class': self.wm_class,
+            'icon': self.icon,
+            'icondata': self.icondata,
+        }
     
     def __str__(self):
         return str( self.to_dict() )
