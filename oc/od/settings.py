3172be9db6ef7edbd14f77eacbb05ca4177110d3
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2025-11-14 14:46:28 +0100

    fix volume

--- oc/od/settings.py
+++ oc/od/settings.py
@@ -427,12 +427,14 @@ def init_desktop():
         }
     if not isinstance ( desktop_pod.get('graphical', {}).get('volumes') , list ):
         desktop_pod['graphical']['volumes'] = [ 'x11socket', 'pulseaudiosocket', 'cupsdsocket', 'tmp', 'run', 'log', 'rundbus', 'runuser' ]
+        logger.debug(f"fixing desktop.pod.graphical.volumes config {desktop_pod['graphical']['volumes']}")
     if not isinstance ( desktop_pod.get('ephemeral_container', {}).get('volumes') , list ):
         # ephemeral container use the same volumes as graphical pod
         desktop_pod['ephemeral_container']['volumes'] = desktop_pod['graphical']['volumes']
+        logger.debug(f"fixing desktop.pod.ephemeral_container.volumes config {desktop_pod['ephemeral_container']['volumes']}")
     if not isinstance ( desktop_pod.get('pod_application', {}).get('volumes') , list ):
-        desktop_pod['graphical']['volumes'] = [ 'tmp', 'run', 'log', 'rundbus', 'runuser' ]
-
+        desktop_pod['pod_application']['volumes'] = [ 'tmp', 'run', 'log', 'rundbus', 'runuser' ]
+        logger.debug(f"fixing desktop.pod.pod_application.volumes config {desktop_pod['pod_application']['volumes']}")  
     init_balloon()
 
     # apply cgroup memory and cpu 
