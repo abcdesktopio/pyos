f87919349fd91d216459e92c331800d969018d36
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2026-01-12 15:29:55 +0100

    fix if no code

--- oc/od/apps.py
+++ oc/od/apps.py
@@ -746,6 +746,11 @@ class ODApps:
                 if self.thread_event.is_set():
                     break
 
+                # If the exception has no code, we cannot handle it
+                if hasattr(e, 'code') is False:
+                    self.thread_event.set()
+                    break 
+
                 # The $changeStream stage is only supported on replica sets
                 if e.code == 40573:
                     self.thread_event.set()
