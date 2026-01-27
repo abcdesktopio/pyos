526cbc821eda5d7827717c3a45df6e865da1235b
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2026-01-27 14:15:32 +0100

    update asn

--- oc/od/asnumber.py
+++ oc/od/asnumber.py
@@ -7,25 +7,30 @@ logger = logging.getLogger(__name__)
 @oc.logging.with_logger()
 class ODASNumber:
 
-    def __init__(self, database:str='ipasn_db.dat', enable:bool=True ):
-        self.asndb = None
-        self.enable = enable
-        if self.enable is True:
-            try:
-                self.asndb = pyasn.pyasn(database)
-            except Exception as e:
-                self.logger.error( f"Error while loading ASNumber database {database} : {e}" )
-                self.asndb = None
+    def __init__(self, database:str='ipasn_db.dat' ):
+        self.asndb = None  
+        try:
+            self.asndb = pyasn.pyasn(database)
+        except Exception as e:
+            self.logger.error( f"Error while loading ASNumber database {database} : {e}" )
 
     def isinitialized(self):
         return isinstance( self.asndb, pyasn.pyasn)
 
+
+    def getasn( self, ipaddr:str ) -> str:
+        asn = None
+        if not self.isinitialized():
+            self.logger.error( "ASNumber database not initialized" )
+            return asn
+        try:
+            asn, prefix = self.asndb.lookup( ipaddr )
+        except Exception as e:
+            self.logger.error( e )
+        return str(asn) if asn is not None else None
+
     def lookup( self, ipaddr:str, asnumber )->bool:
         bReturn = False
-        if not self.enable:
-            self.logger.error( "ASNumber is disabled" )
-            return bReturn
-        
         if not self.isinitialized():
             self.logger.error( "ASNumber database not initialized" )
             return bReturn
