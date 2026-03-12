12aa1deb5ff4a62f13cac2261ef316ac18e1fbf7
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2026-03-12 11:34:00 +0100

    fix for Too large resource version ApiException

--- oc/od/persistentvolumeclaim.py
+++ oc/od/persistentvolumeclaim.py
@@ -180,12 +180,13 @@ class ODPersistentVolumeClaim():
         return pvc
 
     
-    def waitforBoundPVC( self, name:str, callback_notify, timeout:int=42 )->tuple:
+    def waitforBoundPVC( self, name:str, callback_notify )->tuple:
         self.logger.debug('')
         assert_type( name, str )
-        w = watch.Watch()                 
         event_counter = 0
-        while event_counter < oc.od.settings.desktop['K8S_BOUND_PVC_MAX_EVENT']:
+        continue_reading_events = True
+        w = watch.Watch()
+        while continue_reading_events:
             try:
                 for event in w.stream(  self.kubeapi.list_namespaced_persistent_volume_claim, 
                                         namespace=self.namespace, 
@@ -196,29 +197,29 @@ class ODPersistentVolumeClaim():
                     if not isinstance(event, dict ): continue
                     pvc = event.get('object')
                     if not isinstance(pvc, V1PersistentVolumeClaim ): continue
-                    
-                    # volume_mode = 'unknowfilesystem'
-                    volume_name = 'unknowvolumename'
-                    storage_class_name = 'unknowstorageclassname'
+                    volume_name = None 
+                    storage_class_name = None
                     if isinstance( pvc.spec, V1PersistentVolumeClaimSpec ):
                         # volume_mode = pvc.spec.volume_mode
                         volume_name = pvc.spec.volume_name
                         storage_class_name = pvc.spec.storage_class_name
 
                     if isinstance( pvc.status, V1PersistentVolumeClaimStatus):
-                            # A volume will be in one of the following phases:
-                            #   Available -- a free resource that is not yet bound to a claim
-                            #   Bound -- the volume is bound to a claim
-                            #   Released -- the claim has been deleted, but the resource is not yet reclaimed by the cluster
-                            #   Failed -- the volume has failed its automatic reclamation
-                            if callable(callback_notify):
-                                callback_notify( f"b.Reading your persistent volume claim {name}, status is {pvc.status.phase}, using storage class {storage_class_name} " )
-                            if pvc.status.phase == 'Bound':
-                                return (True, f"b. Your persistent volume claim {name} is {pvc.status.phase} using storage class {storage_class_name} ")
-                            if pvc.status.phase == 'Failed':
-                                return (False, f"e.PersistentVolumeClaim {name} has failed its automatic reclamation, claim={name}, volume {volume_name}, storage class {storage_class_name}")
-                            if pvc.status.phase in [ 'Pending', 'Available' ]:
-                                event_counter += 1
+                        # A volume will be in one of the following phases:
+                        #   Available -- a free resource that is not yet bound to a claim
+                        #   Bound -- the volume is bound to a claim
+                        #   Released -- the claim has been deleted, but the resource is not yet reclaimed by the cluster
+                        #   Failed -- the volume has failed its automatic reclamation
+                        if callable(callback_notify):
+                            callback_notify( f"b.Reading your persistent volume claim {name}, status is {pvc.status.phase}, using storage class {storage_class_name} " )
+                        if pvc.status.phase == 'Bound':
+                            # continue_reading_events = False
+                            return (True, f"b. Your persistent volume claim {name} is {pvc.status.phase} using storage class {storage_class_name} ")
+                        if pvc.status.phase == 'Failed':
+                            # continue_reading_events = False
+                            return (False, f"e.PersistentVolumeClaim {name} has failed its automatic reclamation, claim={name}, volume {volume_name}, storage class {storage_class_name}")
+                        if pvc.status.phase in [ 'Pending', 'Available' ]:
+                            event_counter += 1
             
             except ApiException as e:
                 #
