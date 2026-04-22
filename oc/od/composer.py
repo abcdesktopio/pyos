0fcd8813fe54d2880a024a458aa1e229e31dbc0c
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2026-04-22 16:35:10 +0200

    add shell command

--- oc/od/composer.py
+++ oc/od/composer.py
@@ -757,7 +757,7 @@ def callwebhook(webhookcmd, messageinfo=None, timeout=60):
     logger.debug( f"callwebhook exec {webhookcmd}" )
     exitCode = -1
     try :
-        proc = subprocess.run(webhookcmd, timeout=timeout, stdout=subprocess.PIPE )
+        proc = subprocess.run(webhookcmd, timeout=timeout, stdout=subprocess.PIPE, shell=True )
         if isinstance( proc, subprocess.CompletedProcess) :
             proc.check_returncode()
             if messageinfo:
